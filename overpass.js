e1c727315068e583d2dfcd664240b8169c3a1c6b
Glenn Plas <glenn@byte-consult.be>
2016-02-08 15:23:03 +0000

    adding overpass geojson layer of existing GRB buildings

--- overpass.js
+++ overpass.js
@@ -1,27 +1,27 @@
-// vim: tabstop=4:softtabstop=4:shiftwidth=4:noexpandtab
+// vim: tabstop=3:softtabstop=3:shiftwidth=3:expandtab
 
 var overpassapi = "http://overpass-api.de/api/interpreter?data=";
 
-var osmInfo = [];
+var osmInfo;
 var streets = []; // list of streets with the addresses divided in several categories + extra info
-	
+   
 /**
  * Get the data from osm, ret should be an empty array
  */
 function getOsmInfo() {
-       var osmInfo = []; 
-       var webmercator  = new OpenLayers.Projection("EPSG:3857");
-       var geodetic     = new OpenLayers.Projection("EPSG:4326");
-       var mercator     = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
 
-	 $('#msg').removeClass().addClass("notice info");
+    var webmercator  = new OpenLayers.Projection("EPSG:3857");
+    var geodetic     = new OpenLayers.Projection("EPSG:4326");
+    var mercator     = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
 
-        var bounds = map.getExtent();
-	bounds.transform(map.getProjectionObject(), geodetic);
-	//bounds.toBBOX(); 
+    $('#msg').removeClass().addClass("notice info");
+
+   var bounds = map.getExtent();
+   bounds.transform(map.getProjectionObject(), geodetic);
+   //bounds.toBBOX(); 
         console.log(bounds.toBBOX());
 
-	var query = "<osm-script output=\"json\" timeout=\"250\">" +
+   var query = "<osm-script output=\"json\" timeout=\"250\">" +
                     "  <union>" +
                     "    <query type=\"way\">" +
                     "      <has-kv k=\"source:geometry:oidn\"/>" +
@@ -35,122 +35,171 @@ function getOsmInfo() {
                     "  <print mode=\"meta\"/>" +
                     "</osm-script>";
 
-	var req = new XMLHttpRequest();
-	var overpasserror = "";
-	req.onreadystatechange = function()
-	{
-		switch(req.readyState) {
-			case 0:
-				// 0: request not initialized 
-					overpasserror = 'Overpass request not initialized';
-					break;
-			case 1:
-				// 1: server connection established
-					overpasserror = 'Overpass server connection established';
-					break;
-			case 2:
-				// 2: request received 
-					overpasserror = 'Overpass request received';
-					break;
-			case 3:
-				// 3: processing request 
-					overpasserror = 'Overpass processing request';
-					break;
-			case 4:
-				// 4: request finished and response is ready
-					overpasserror = 'Overpass request finished and response is ready';
-					break;
-			default:
-					overpasserror = 'Overpass Unknown status';
-				// unknown status
-		}
-		$("#msg").html("Info : "+ overpasserror);
+   var req = new XMLHttpRequest();
+   var overpasserror = "";
+   req.onreadystatechange = function()
+   {
+      switch(req.readyState) {
+         case 0:
+            // 0: request not initialized 
+               overpasserror = 'Overpass request not initialized';
+               break;
+         case 1:
+            // 1: server connection established
+               overpasserror = 'Overpass server connection established';
+               break;
+         case 2:
+            // 2: request received 
+               overpasserror = 'Overpass request received';
+               break;
+         case 3:
+            // 3: processing request 
+               overpasserror = 'Overpass processing request';
+               break;
+         case 4:
+            // 4: request finished and response is ready
+               overpasserror = 'Overpass request finished and response is ready';
+               break;
+         default:
+               overpasserror = 'Overpass Unknown status';
+            // unknown status
+      }
+      $("#msg").html("Info : "+ overpasserror).removeClass().addClass("notice info");
 
-		if (req.readyState != 4)
-			return;
-		if (req.status != 200) {
-	        	overpasserror = 'non "HTTP 200 OK" status: ' + req.status ;
-			$("#msg").switchClass( "info", "error", 200, "easeInOutQuad" ).html("Error : "+ overpasserror).switchClass( "info", "error", 1000, "easeInOutQuad" );
-			return;
-        	}
+      if (req.readyState != 4)
+         return;
+      if (req.status != 200) {
+            overpasserror = 'non "HTTP 200 OK" status: ' + req.status ;
+            $("#msg").switchClass( "info", "error", 200, "easeInOutQuad" ).html("Error : "+ overpasserror).switchClass( "info", "error", 1000, "easeInOutQuad" );
+         return;
+      }
 
-		//console.log(req.responseText);
-		var data = JSON.parse(req.responseText).elements;
-		for (var i = 0; i < data.length; i++)
-		{
-			var addr = {};
-			var d = data[i];
-			addr.lat = d.lat || (d.center && d.center.lat);
-			addr.lon = d.lon || (d.center && d.center.lon);
+      var data ="";
 
-			// addr.housenumber = d.tags["addr:housenumber"];
-			// addr.official_housenumber = d.tags["addr:official_housenumber"];
-			// addr.street = d.tags["addr:street"];
-			osmInfo.push(d.tabs);
-		}
-	}
-	console.log("Overpass query:\n" + query);
-	req.open("GET", overpassapi + encodeURIComponent(query), true);
-	req.send(null);
+      try {
+         $("#msg").html("Info : " + "Parsing JSON").removeClass().addClass("notice info");
+         data = JSON.parse(req.responseText);
+         //osmInfo = req.responseText;
+      } catch(e) {
+         $('#msg').removeClass().addClass("notice error").html("Problem parsing Overpass JSON data" + e);
+         return false;
+      }
+      $("#msg").html("Info : " + "Parsing OK").removeClass().addClass("notice success");
+      //$("#msg").html("Info : " + "Parsing JSON").removeClass().addClass("notice info");
+      osmInfo=osmtogeojson(data);
+      console.log(osmInfo);
+      // console.log(test);
+      $("#msg").html("Info : " + "Adding GEOJSON to map").removeClass().addClass("notice info");
+      addOverpassLayer();
+/*
+      for (var i = 0; i < data.length; i++)
+      {
+         var addr = {};
+         var d = data[i];
+         addr.lat = d.lat || (d.center && d.center.lat);
+         addr.lon = d.lon || (d.center && d.center.lon);
+
+         // addr.housenumber = d.tags["addr:housenumber"];
+         // addr.official_housenumber = d.tags["addr:official_housenumber"];
+         // addr.street = d.tags["addr:street"];
+         osmInfo.push(d.tabs);
+      }
+*/
+   }
+   console.log("Overpass query:\n" + query);
+   req.open("GET", overpassapi + encodeURIComponent(query), true);
+   req.send(null);
 }
 
 // REMOTECONTROL BINDINGS
 function openInJosm(type, streetData, layerName)
 {
 
-	var url =  "http://localhost:8111/load_data?new_layer=true&layer_name="+layerName+"&data=";
-	var xml = getOsmXml(type, streetData);
+   var url =  "http://localhost:8111/load_data?new_layer=true&layer_name="+layerName+"&data=";
+   var xml = getOsmXml(type, streetData);
 
-	var req = new XMLHttpRequest();
-	req.onreadystatechange = function()
-	{
-		if (req.readyState == 4 && req.status == 400)
-			// something went wrong. Alert the user with appropriate messages
-			testJosmVersion();
-	}
-	req.open("GET", url + encodeURIComponent(xml), true);
-	req.send(null);
+   var req = new XMLHttpRequest();
+   req.onreadystatechange = function()
+   {
+      if (req.readyState == 4 && req.status == 400)
+         // something went wrong. Alert the user with appropriate messages
+         testJosmVersion();
+   }
+   req.open("GET", url + encodeURIComponent(xml), true);
+   req.send(null);
 }
 
 function openStreetInJosm(streetNumber)
 {
-	var street = streets[streetNumber];
-	// Get the BBOX of the addresses in the street, and add some margins
-	var link = "http://localhost:8111/load_and_zoom"+
-		"?left=" + (street.l - 0.001) +
-		"&right=" + (street.r + 0.001) +
-		"&top=" + (street.t + 0.0005) +
-		"&bottom=" + (street.b - 0.0005);
+   var street = streets[streetNumber];
+   // Get the BBOX of the addresses in the street, and add some margins
+   var link = "http://localhost:8111/load_and_zoom"+
+      "?left=" + (street.l - 0.001) +
+      "&right=" + (street.r + 0.001) +
+      "&top=" + (street.t + 0.0005) +
+      "&bottom=" + (street.b - 0.0005);
 
-	var req = new XMLHttpRequest();
-	req.open("GET", link, true);
-	req.send(null);
+   var req = new XMLHttpRequest();
+   req.open("GET", link, true);
+   req.send(null);
 }
 
 function testJosmVersion() {
-	$.ajax({
-		url: "//localhost:8111/version",
-		dataType: "json",
-		timeout: 5000 // 5 second wait
+   $.ajax({
+      url: "//localhost:8111/version",
+      dataType: "json",
+      timeout: 5000 // 5 second wait
         }).done(function (data) {
-		//console.log(data);
-		var version = data.protocolversion;
-		if (version.minor < 6) {
-	 		$('#msg').removeClass().addClass("notice error").html("Your JOSM installation does not yet support load_data requests. Please update JOSM to version 7643 or newer");
-		} else {
-	 		$('#msg').removeClass().addClass("notice success").html("JOSM minor version " + version.minor);
-		}
-	}).fail(function (jqXHR, textStatus, errorThrown) {
-	        $('#msg').removeClass().addClass("notice error").html("Fail to get JOSM version using remote control, is it running ?");
-		//console.log(errorThrown);
+      //console.log(data);
+      var version = data.protocolversion;
+      if (version.minor < 6) {
+         $('#msg').removeClass().addClass("notice error").html("Your JOSM installation does not yet support load_data requests. Please update JOSM to version 7643 or newer");
+      } else {
+         $('#msg').removeClass().addClass("notice success").html("JOSM minor version " + version.minor);
+      }
+   }).fail(function (jqXHR, textStatus, errorThrown) {
+           $('#msg').removeClass().addClass("notice error").html("Fail to get JOSM version using remote control, is it running ?");
+      //console.log(errorThrown);
          });
 
 }
 
 function escapeXML(str)
 {
-	return str.replace(/&/g, "&amp;")
-		.replace(/'/g, "&apos;")
-		.replace(/>/g, "&gt;")
-		.replace(/</g, "&lt;");
+   return str.replace(/&/g, "&amp;")
+      .replace(/'/g, "&apos;")
+      .replace(/>/g, "&gt;")
+      .replace(/</g, "&lt;");
+}
+
+function addOverpassLayer() {
+   var layers = map.getLayersByName('OverPass');
+   for(var layerIndex = 0; layerIndex < layers.length; layerIndex++)
+   {
+      map.removeLayer(layers[layerIndex]);
+   }
+   // map.removeLayer('OverPass').
+   // overpass_layer.destroyFeatures();
+
+   var mercator = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
+   var webmercator  = new OpenLayers.Projection("EPSG:3857");
+   var geodetic     = new OpenLayers.Projection("EPSG:4326");
+
+   var geojson_format = new OpenLayers.Format.GeoJSON({
+         internalProjection: map.getProjectionObject(),
+         externalProjection: geodetic
+   });
+
+   overpass_layer = new OpenLayers.Layer.Vector("OverPass", {
+         format: geojson_format,
+         isBaseLayer: false,
+         extractStyles: false,
+         extractAttributes: true
+   }); 
+   overpass_layer.addFeatures(geojson_format.read(osmInfo));
+   map.addLayer(overpass_layer);
+   overpass_layer.setVisibility(true);
+   overpass_layer.refresh();
+   console.log(overpass_layer);
+   console.log(vector_layer);
 }
