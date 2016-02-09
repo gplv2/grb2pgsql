337c3ef46306a36e6c5d48691555b8a18b32e445
Glenn Plas <glenn@byte-consult.be>
2016-02-09 20:38:49 +0000

    switch off gzip as it seems to be problematic with vary-encoding

--- postgis_geojson.php
+++ postgis_geojson.php
@@ -1,4 +1,8 @@
 <?php
+
+/** Error reporting */
+error_reporting(E_ALL);
+
 // vim: tabstop=3:softtabstop=3:shiftwidth=3:noexpandtab
 
 /**
@@ -18,15 +22,22 @@
  * @param      string      $offset     Offset used in conjunction with limit *OPTIONAL*
  * @return     string                  geojson string
  */
-ini_set('zlib.output_compression', 1);
+//ini_set('zlib.output_compression', 1);
 
 // http://jibbering.com/blog/?p=514
 
+
+// I'm serious about not wanting any client or proxy caching..
 header('Content-Type: application/json');
+$headexpires = gmdate('D, d M Y H:i:s') . " GMT";
+header("Last-Modified: " . $headexpires);
+header("Pragma: no-cache");
+header("Expires: " . $headexpires);
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
 
 //@mb_internal_encoding('UTF-8');
 //setlocale(LC_ALL, 'en_US.UTF-8');
-
 
 function escapeJsonString($value) { # list from www.json.org: (\b backspace, \f formfeed)
   $escapers = array("\\", "/", "\"", "\n", "\r", "\t", "\x08", "\x0c");
@@ -38,47 +49,57 @@ function escapeJsonString($value) { # list from www.json.org: (\b backspace, \f 
 $geotable = 'planet_osm_polygon';
 $geomfield = 'way';
 
-if (empty($_REQUEST['srid'])) {
-    $srid = '900913';
-    //$srid = '3857';
-    //$srid = '4326';
+
+/* test if we are called from the CLI */
+if (!defined('STDIN')) {
+
+   if (empty($_REQUEST['srid'])) {
+      $srid = '900913';
+      //$srid = '3857';
+      //$srid = '4326';
+   } else {
+      $srid = $_REQUEST['srid'];
+   }
+
+   if (empty($_REQUEST['fields'])) {
+      $fields = '*';
+   } else {
+      $fields = $_REQUEST['fields'];
+   }
+
+   if (!empty($_REQUEST['parameters'])) {
+      $parameters = $_REQUEST['parameters'];
+   }
+
+   if (!empty($_REQUEST['orderby'])) {
+      $orderby    = $_REQUEST['orderby'];
+   }
+
+   if (empty($_REQUEST['sort'])) {
+      $sort = 'ASC';
+   } else {
+      $sort = $_REQUEST['sort'];
+   }
+
+   if (!empty($_REQUEST['limit'])) {
+      $limit      = $_REQUEST['limit'];
+   }
+
+   if (!empty($_REQUEST['offset'])) {
+      $offset     = $_REQUEST['offset'];
+   }
+
+   if (!empty($_REQUEST['bbox'])) {
+      $bbox = $_REQUEST['bbox'];
+      list($bbox_west, $bbox_south, $bbox_east, $bbox_north) = preg_split("/,/", $bbox);
+   }
 } else {
-    $srid = $_REQUEST['srid'];
+   $fields = '*';
+   $srid = '900913';
+   $bbox="382234.25632491,6566593.3158132,669484.60858063,6697453.5082192";
+   list($bbox_west, $bbox_south, $bbox_east, $bbox_north) = preg_split("/,/", $bbox);
+   list($bbox['south'], $bbox['west'], $bbox['east'], $bbox['north']) = preg_split("/,/", $bbox['full']); // west, south, east, north
 }
-
-if (empty($_REQUEST['fields'])) {
-    $fields = '*';
-} else {
-    $fields = $_REQUEST['fields'];
-}
-
-if (!empty($_REQUEST['parameters'])) {
-   $parameters = $_REQUEST['parameters'];
-}
-
-if (!empty($_REQUEST['orderby'])) {
-   $orderby    = $_REQUEST['orderby'];
-}
-
-if (empty($_REQUEST['sort'])) {
-    $sort = 'ASC';
-} else {
-    $sort = $_REQUEST['sort'];
-}
-   
-if (!empty($_REQUEST['limit'])) {
-   $limit      = $_REQUEST['limit'];
-}
-
-if (!empty($_REQUEST['offset'])) {
-   $offset     = $_REQUEST['offset'];
-}
-
-if (!empty($_REQUEST['bbox'])) {
-   $bbox = $_REQUEST['bbox'];
-   list($bbox_west, $bbox_south, $bbox_east, $bbox_north) = split(",", $bbox);
-}
-
 # Build SQL SELECT statement and return the geometry as a GeoJSON element in EPSG: 4326
 $sql  = "SELECT " . pg_escape_string($fields) . ", st_asgeojson(ST_Transform(" . pg_escape_string($geomfield) . ",$srid)) AS geojson FROM " . pg_escape_string($geotable);
 $sql .= sprintf(" WHERE " . pg_escape_string("way") . " && ST_SetSRID('BOX3D(%s %s, %s %s)'::box3d, %s)", $bbox_west, $bbox_south, $bbox_east, $bbox_north, $srid);
@@ -95,6 +116,8 @@ if (!empty($limit)){
 if (!empty($offset)){
     $sql .= " OFFSET " . pg_escape_string($offset);
 }
+
+//echo $sql;
 
 $redis = new Redis();
 $redis->connect('127.0.0.1'); // port 6379 by default
