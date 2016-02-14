78ef645d28515e93ab1ad15930a15ad420048076
Glenn Plas <glenn@byte-consult.be>
2016-02-14 02:11:30 +0000

    fixed reprojection issues with ogr2ogr output and ogr2osm not detecting srs"

--- up/server/work/parse.sh
+++ up/server/work/parse.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#for file in G*/Shapefile/Gbg*.shp
-for file in G*/Shapefile/Gbg23096B500.shp
+for file in G*/Shapefile/Gbg*.shp
+#for file in G*/Shapefile/Gbg23096B500.shp
 
 do
  echo "Processing $file"
@@ -9,16 +9,19 @@ do
  extension="${filename##*.}"
  filename="${filename%.*}"
 
-#echo $dirname
+ echo $dirname
+ echo "Cleanup parsed"
+ echo "=============="
  rm -Rf "*_parsed"
  echo "OGR FILE INFO"
  echo "============="
  /usr/local/bin/ogrinfo -al -so ${dirname}/${filename}.shp
- echo -n ""
+ echo ""
 
  echo "OGR2OGR"
  echo "======="
  echo /usr/local/bin/ogr2ogr -s_srs "EPSG:31370" -t_srs "EPSG:4326" "${filename}_parsed" ${dirname}/${filename}.shp -overwrite
+ /usr/local/bin/ogr2ogr -s_srs "EPSG:31370" -t_srs "EPSG:4326" "${filename}_parsed" ${dirname}/${filename}.shp -overwrite
  #/usr/local/bin/ogr2ogr -overwrite -t_srs EPSG:3857 -s_srs "ESRI::${dirname}/${filename}.prj" "${filename}_parsed" ${dirname}/${filename}.shp
  #/usr/local/bin/ogr2ogr -overwrite -t_srs EPSG:900931 -s_srs "ESRI::${dirname}/${filename}.prj" "${filename}_parsed" ${dirname}/${filename}.shp
  #/usr/local/bin/ogr2ogr -overwrite -t_srs EPSG:900931 "${filename}_parsed" ${dirname}/${filename}.shp
@@ -26,13 +29,13 @@ do
  
  #ogr2osm.py Gbg11024B500_parsed/Gbg11024B500.shp
 
- echo -n ""
+ echo ""
  echo "OGR2OSM"
  echo "======="
  rm -f "${filename}.osm"
- echo ogr2osm.py "${filename}_parsed/${filename}.shp"
- ogr2osm.py "${filename}_parsed/${filename}.shp"
- echo -n ""
+ echo /usr/local/bin/ogr2osm/ogr2osm.py "${filename}_parsed/${filename}.shp"
+ /usr/local/bin/ogr2osm/ogr2osm.py "${filename}_parsed/${filename}.shp"
+ echo ""
 
  echo "GRB2OSM"
  echo "======="
@@ -44,17 +47,16 @@ do
 
  # echo -n $file
  # do something on $f
- echo -n ""
+ echo ""
  echo "IMPORT"
  echo "======"
- echo osm2pgsql --slim --create --cache 1000 --number-processes 2 --hstore --style /usr/local/src/osm/openstreetmap-carto/openstreetmap-carto.style --multi-geometry -d grb -U grb-data "${filename}_addressed.osm"
- osm2pgsql --slim --create --cache 1000 --number-processes 2 --hstore --style /usr/local/src/osm/openstreetmap-carto/openstreetmap-carto.style --multi-geometry -d grb -U grb-data "${filename}_addressed.osm"
+ echo /usr/bin/osm2pgsql --slim --create --cache 1000 --number-processes 2 --hstore --style /usr/local/src/osm/openstreetmap-carto/openstreetmap-carto.style --multi-geometry -d grb -U grb-data "${filename}_addressed.osm"
+ /usr/bin/osm2pgsql --slim --create --cache 1000 --number-processes 2 --hstore --style /usr/local/src/osm/openstreetmap-carto/openstreetmap-carto.style --multi-geometry -d grb -U grb-data "${filename}_addressed.osm"
 
- echo -n ""
+ echo ""
  echo "Flush cache"
  echo "==========="
  # flush redish cache
- echo "flushall" | redis-cli 
-exit;
 done
+echo "flushall" | redis-cli 
  
